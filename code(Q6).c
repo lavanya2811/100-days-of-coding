b9ba80749ed112e3b1350dfb3d33a50377b3b5f4
lavanya2811 <lavanya.32727@stu.upes.ac.in>
2026-09-13 17:28:14 +0530

    Add program to swap two numbers
    This program swaps two numbers using a third variable.

---
+++ code(Q6).c
@@ -0,0 +1,23 @@
+//Write a program to swap two numbers using a third variable.
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, temp;
+
+    printf("Enter number 1:\n");
+    scanf("%d", &num1);
+
+    printf("Enter number 2:\n");
+    scanf("%d", &num2);
+
+    printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
+
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
+
+    return 0;
+}
